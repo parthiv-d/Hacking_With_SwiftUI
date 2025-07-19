fe606608760ff5c74a5f71de13594f6f60a6e354
Parthiban D <parthiban.d@stellantis.com>
2025-07-19 16:48:29 +0530

    Added @Namespace Property Wrapper for Practice.

--- EnvironmentObjectTraining/EnvironmentObjectTraining/HomeView.swift
+++ EnvironmentObjectTraining/EnvironmentObjectTraining/HomeView.swift
@@ -10,11 +10,39 @@ import SwiftUI
 struct HomeView: View {
     @EnvironmentObject var session: SessionManager
 
+    @Namespace private var animation
+    @State private var isExpanded = false
+    
     var body: some View {
         VStack {
             Text("Welcome, \(session.username)")
                 .font(.title)
                 .padding()
+            
+            VStack {
+                if !isExpanded {
+                    RoundedRectangle(cornerRadius: 25)
+                        .fill(Color.blue)
+                        .matchedGeometryEffect(id: "box", in: animation)
+                        .frame(width: 100, height: 100)
+                }
+
+                Spacer()
+
+                if isExpanded {
+                    RoundedRectangle(cornerRadius: 25)
+                        .fill(Color.red)
+                        .matchedGeometryEffect(id: "box", in: animation)
+                        .frame(width: 300, height: 300)
+                }
+
+                Button("Toggle") {
+                    withAnimation(.snappy) {
+                        isExpanded.toggle()
+                    }
+                }
+            }
+            .padding()
 
             Button("Logout") {
                 session.logout()
